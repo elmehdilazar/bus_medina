d8ef0d87b3490e3391492516f30c6484b0ffb911
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2023-01-11 22:37:16 +0100

    client
    add jpanel

--- src/bus2/client.java
+++ src/bus2/client.java
@@ -2,22 +2,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-<<<<<<< HEAD:src/bus2/client/client.java
-package bus2.client;
-import bus2.client.*;
-=======
+
 package bus2;
 
->>>>>>> parent of a36fc5c (Merge pull request #5 from elmehdilazar/client):src/bus2/client.java
+
+
+
+
 import java.awt.Color;
 import java.awt.Component;
+
+import java.awt.Component;
+
+import java.awt.Color;
+import java.awt.PopupMenu;
+import javax.swing.JPanel;
 
 /**
  *
  * @author IGA_BELV
  */
 public class client extends javax.swing.JFrame {
-
+private static client cli;
     /**
      * Creates new form client
      */
@@ -25,7 +31,8 @@ public class client extends javax.swing.JFrame {
         initComponents();
       //  jPanel1.setOpaque(false);
    //  jPanel1.setBackground(new Color(0, 0, 0, 128));
-  
+   cli=this;
+        showForm(new home(cli));
     }
 
     /**
@@ -42,24 +49,6 @@ public class client extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
-        roundPanel1 = new Dashboard.swing.RoundPanel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel15 = new javax.swing.JLabel();
-        roundPanel2 = new Dashboard.swing.RoundPanel();
-        jLabel11 = new javax.swing.JLabel();
-        jLabel16 = new javax.swing.JLabel();
-        roundPanel3 = new Dashboard.swing.RoundPanel();
-        jLabel10 = new javax.swing.JLabel();
-        jLabel8 = new javax.swing.JLabel();
-        roundPanel4 = new Dashboard.swing.RoundPanel();
-        jLabel13 = new javax.swing.JLabel();
-        jLabel18 = new javax.swing.JLabel();
-        roundPanel5 = new Dashboard.swing.RoundPanel();
-        jLabel12 = new javax.swing.JLabel();
-        jLabel17 = new javax.swing.JLabel();
-        roundPanel6 = new Dashboard.swing.RoundPanel();
-        jLabel9 = new javax.swing.JLabel();
-        jLabel14 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
@@ -105,103 +94,7 @@ public class client extends javax.swing.JFrame {
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1080, 70));
 
         jPanel2.setOpaque(false);
-        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        roundPanel1.setBackground(new Color(255,224,25,179));
-        roundPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        jLabel6.setBackground(new java.awt.Color(255, 224, 25));
-        jLabel6.setFont(new java.awt.Font("Book Antiqua", 1, 18)); // NOI18N
-        jLabel6.setForeground(new java.awt.Color(159, 161, 159));
-        jLabel6.setText("les lignes");
-        roundPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 40, 90, -1));
-
-        jLabel15.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel15.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bus2/icon/icons8-driveway-60.png"))); // NOI18N
-        roundPanel1.add(jLabel15, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, -1, 60));
-
-        jPanel2.add(roundPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(426, 129, 230, 110));
-
-        roundPanel2.setBackground(new Color(255,224,25,179));
-        roundPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        jLabel11.setBackground(new java.awt.Color(255, 224, 25));
-        jLabel11.setFont(new java.awt.Font("Book Antiqua", 1, 18)); // NOI18N
-        jLabel11.setForeground(new java.awt.Color(159, 161, 159));
-        jLabel11.setText("les quartiers");
-        roundPanel2.add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 40, 110, -1));
-
-        jLabel16.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel16.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bus2/icon/icons8-company-60.png"))); // NOI18N
-        roundPanel2.add(jLabel16, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, -1, -1));
-
-        jPanel2.add(roundPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(757, 129, 230, 110));
-
-        roundPanel3.setBackground(new Color(255,224,25,179));
-        roundPanel3.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        jLabel10.setBackground(new java.awt.Color(255, 224, 25));
-        jLabel10.setFont(new java.awt.Font("Book Antiqua", 1, 18)); // NOI18N
-        jLabel10.setForeground(new java.awt.Color(159, 161, 159));
-        jLabel10.setText("tarif par ligne");
-        roundPanel3.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 40, 120, -1));
-
-        jLabel8.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bus2/icon/icons8-driveway-60-price.png"))); // NOI18N
-        roundPanel3.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 20, -1, -1));
-
-        jPanel2.add(roundPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(95, 315, 230, 110));
-
-        roundPanel4.setBackground(new Color(255,224,25,179));
-        roundPanel4.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        jLabel13.setBackground(new java.awt.Color(255, 224, 25));
-        jLabel13.setFont(new java.awt.Font("Book Antiqua", 1, 18)); // NOI18N
-        jLabel13.setForeground(new java.awt.Color(159, 161, 159));
-        jLabel13.setText("nos offres");
-        roundPanel4.add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 40, 110, -1));
-
-        jLabel18.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel18.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bus2/icon/icons8-discount-60.png"))); // NOI18N
-        roundPanel4.add(jLabel18, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, -1, -1));
-
-        jPanel2.add(roundPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(425, 315, 230, 110));
-
-        roundPanel5.setBackground(new Color(255,224,25,179));
-        roundPanel5.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                roundPanel5MouseClicked(evt);
-            }
-        });
-        roundPanel5.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        jLabel12.setBackground(new java.awt.Color(255, 224, 25));
-        jLabel12.setFont(new java.awt.Font("Book Antiqua", 1, 18)); // NOI18N
-        jLabel12.setForeground(new java.awt.Color(159, 161, 159));
-        jLabel12.setText("demandez ");
-        roundPanel5.add(jLabel12, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 40, 100, -1));
-
-        jLabel17.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel17.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bus2/icon/icons8-demand-60.png"))); // NOI18N
-        roundPanel5.add(jLabel17, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 20, -1, -1));
-
-        jPanel2.add(roundPanel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(755, 315, 230, 110));
-
-        roundPanel6.setBackground(new Color(255,224,25,179));
-        roundPanel6.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        jLabel9.setBackground(new java.awt.Color(255, 224, 25));
-        jLabel9.setFont(new java.awt.Font("Book Antiqua", 1, 18)); // NOI18N
-        jLabel9.setForeground(new java.awt.Color(159, 161, 159));
-        jLabel9.setText("station ");
-        roundPanel6.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 40, 90, -1));
-
-        jLabel14.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel14.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bus2/icon/icons8-bus-station-60.png"))); // NOI18N
-        roundPanel6.add(jLabel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, -1, -1));
-
-        jPanel2.add(roundPanel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(94, 129, 230, 110));
-
+        jPanel2.setLayout(new java.awt.BorderLayout());
         getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 70, 1080, 470));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bus2/icon/mirroir.png"))); // NOI18N
@@ -235,16 +128,14 @@ public class client extends javax.swing.JFrame {
             }
         }
     }//GEN-LAST:event_formWindowOpened
-
-    private void roundPanel5MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_roundPanel5MouseClicked
-      Component com=new demende();
+   public  void showForm(Component com) {
         jPanel2.removeAll();
         jPanel2.add(com);
-       jPanel2.revalidate();
-        jPanel2.repaint();
-        
-    }//GEN-LAST:event_roundPanel5MouseClicked
-
+         jPanel2.repaint();
+        jPanel2.revalidate();
+       
+    }
+  
     /**
      * @param args the command line arguments
      */
@@ -282,30 +173,12 @@ public class client extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel10;
-    private javax.swing.JLabel jLabel11;
-    private javax.swing.JLabel jLabel12;
-    private javax.swing.JLabel jLabel13;
-    private javax.swing.JLabel jLabel14;
-    private javax.swing.JLabel jLabel15;
-    private javax.swing.JLabel jLabel16;
-    private javax.swing.JLabel jLabel17;
-    private javax.swing.JLabel jLabel18;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
-    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    private Dashboard.swing.RoundPanel roundPanel1;
-    private Dashboard.swing.RoundPanel roundPanel2;
-    private Dashboard.swing.RoundPanel roundPanel3;
-    private Dashboard.swing.RoundPanel roundPanel4;
-    private Dashboard.swing.RoundPanel roundPanel5;
-    private Dashboard.swing.RoundPanel roundPanel6;
     // End of variables declaration//GEN-END:variables
 }
