0f56687778521951277787317f647f16298d445c
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2023-01-13 00:09:58 +0100

    add admin  button and home button

--- src/bus2/client.java
+++ src/bus2/client.java
@@ -46,8 +46,12 @@ private static client cli;
 
         jPanel1 = new javax.swing.JPanel();
         jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
@@ -78,18 +82,41 @@ private static client cli;
                 jLabel4MouseClicked(evt);
             }
         });
-        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(1029, 15, 20, 30));
-
-        jLabel5.setFont(new java.awt.Font("Book Antiqua", 1, 24)); // NOI18N
-        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel5.setText("bus");
-        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(127, 23, 50, -1));
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(1030, 20, 20, 30));
 
         jLabel7.setBackground(new java.awt.Color(255, 224, 25));
         jLabel7.setFont(new java.awt.Font("Book Antiqua", 1, 24)); // NOI18N
         jLabel7.setForeground(new java.awt.Color(255, 224, 25));
         jLabel7.setText("medina");
         jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(39, 23, 140, -1));
+
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bus2/icon/icons8-home-20.png"))); // NOI18N
+        jLabel6.setAlignmentY(17.0F);
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(830, 20, 20, 30));
+
+        jLabel8.setFont(new java.awt.Font("Book Antiqua", 1, 24)); // NOI18N
+        jLabel8.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel8.setText("bus");
+        jPanel1.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(127, 23, 50, -1));
+
+        jLabel5.setFont(new java.awt.Font("Book Antiqua", 1, 18)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setText("home");
+        jLabel5.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel5MouseClicked(evt);
+            }
+        });
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(860, 20, 60, 30));
+
+        jLabel9.setFont(new java.awt.Font("Book Antiqua", 1, 18)); // NOI18N
+        jLabel9.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel9.setText("admin");
+        jPanel1.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(950, 20, 60, 30));
+
+        jLabel10.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bus2/icon/icons8-customer-20.png"))); // NOI18N
+        jLabel10.setAlignmentY(17.0F);
+        jPanel1.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(920, 20, 20, 30));
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1080, 70));
 
@@ -128,6 +155,10 @@ private static client cli;
             }
         }
     }//GEN-LAST:event_formWindowOpened
+
+    private void jLabel5MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel5MouseClicked
+         showForm(new home(cli));
+    }//GEN-LAST:event_jLabel5MouseClicked
    public  void showForm(Component com) {
         jPanel2.removeAll();
         jPanel2.add(com);
@@ -173,11 +204,15 @@ private static client cli;
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
