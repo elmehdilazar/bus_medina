f66d6f12d22bb120da7cfe23495706cd68dfd493
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2023-01-15 20:45:48 +0100

    modify quartier

--- src/bus2/quartier.java
+++ src/bus2/quartier.java
@@ -32,7 +32,7 @@ private  Connection c;
     c = DriverManager.getConnection("jdbc:mysql://localhost:3306/bus","root","");
         java.sql.ResultSet ab;
             try {
-           updatetab() ;
+           updatetab(null) ;
             } catch (Exception ex) {
                 Logger.getLogger(demende.class.getName()).log(Level.SEVERE, null, ex);
             }
@@ -43,10 +43,17 @@ private  Connection c;
             Logger.getLogger(demende.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    public void updatetab() throws Exception{
+    public void updatetab(String val) throws Exception{
          Connection c = this.c;
-         String sql = "SELECT * FROM `quartier` q INNER join stations s on s.idstations=q.stations_idstations INNER JOIN ligne l on l.id_ligne=s.id_ligne;";
-         PreparedStatement st = c.prepareStatement(sql, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+          String sql;
+         if(val==null || val.isEmpty() || val.trim().isEmpty() || val=="rechercher..."){
+                sql = "SELECT * FROM `quartier` q INNER join stations s on s.idstations=q.stations_idstations INNER JOIN ligne l on l.id_ligne=s.id_ligne;";
+         
+         }else{
+              sql = "SELECT * FROM `quartier` q INNER join stations s on s.idstations=q.stations_idstations INNER JOIN ligne l on l.id_ligne=s.id_ligne where q.nom_quartier like '%"+val.trim()+"%';";
+         
+         }
+       PreparedStatement st = c.prepareStatement(sql, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
          
          ResultSet rs = st.executeQuery();
          ResultSetMetaData rsData = rs.getMetaData();
@@ -80,8 +87,7 @@ private  Connection c;
     private void initComponents() {
 
         roundPanel1 = new Dashboard.swing.RoundPanel();
-        jBtn1 = new Dashboard.component.jBtn();
-        textField1 = new Dashboard.component.TextField();
+        val = new Dashboard.component.TextField();
         jScrollPane1 = new javax.swing.JScrollPane();
         tableAdmin = new Dashboard.form.TableDark();
 
@@ -92,23 +98,20 @@ private  Connection c;
         roundPanel1.setOpaque(true);
         roundPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jBtn1.setText("search");
-        jBtn1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jBtn1ActionPerformed(evt);
+        val.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        val.setText("rechrehcer...");
+        val.setAlignmentX(5.0F);
+        val.addCaretListener(new javax.swing.event.CaretListener() {
+            public void caretUpdate(javax.swing.event.CaretEvent evt) {
+                valCaretUpdate(evt);
             }
         });
-        roundPanel1.add(jBtn1, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 22, 120, 30));
-
-        textField1.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
-        textField1.setText("      search");
-        textField1.setAlignmentX(5.0F);
-        textField1.addActionListener(new java.awt.event.ActionListener() {
+        val.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                textField1ActionPerformed(evt);
+                valActionPerformed(evt);
             }
         });
-        roundPanel1.add(textField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 20, 200, 32));
+        roundPanel1.add(val, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, 200, 32));
 
         tableAdmin.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -128,20 +131,23 @@ private  Connection c;
         add(roundPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 20, 680, 400));
     }// </editor-fold>//GEN-END:initComponents
 
-    private void textField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textField1ActionPerformed
+    private void valActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_valActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_textField1ActionPerformed
+    }//GEN-LAST:event_valActionPerformed
 
-    private void jBtn1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtn1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jBtn1ActionPerformed
+    private void valCaretUpdate(javax.swing.event.CaretEvent evt) {//GEN-FIRST:event_valCaretUpdate
+             try {
+        updatetab(val.getText());
+    } catch (Exception ex) {
+        Logger.getLogger(ligne.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    }//GEN-LAST:event_valCaretUpdate
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private Dashboard.component.jBtn jBtn1;
     private javax.swing.JScrollPane jScrollPane1;
     private Dashboard.swing.RoundPanel roundPanel1;
     private Dashboard.form.TableDark tableAdmin;
-    private Dashboard.component.TextField textField1;
+    private Dashboard.component.TextField val;
     // End of variables declaration//GEN-END:variables
 }
